afc034641188c6c58c081de5631e5ee8e66a4351
instance <zhuravlev.serj2015@yandex.ru>
2025-10-07 15:54:49 +0500

    add_RealEstateObjects.cs add_PropertyPurpose.cs add_PropertyType.cs

---
+++ RealEstateAgency/RealEstateAgency.Domain/Entities/RealEstateObject.cs
@@ -0,0 +1,21 @@
+﻿using RealEstateAgency.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateAgency.Domain.Entities;
+public class RealEstateObject
+{
+    public required string CadastralNumber { get; set; }
+    public required string Address { get; set; }
+    public required int Floors { get; set; }
+    public required double TotalArea { get; set; }
+    public required int Rooms { get; set; }
+    public required double CeilingHeight { get; set; }
+    public required int FloorNumber { get; set; }
+    public required bool HasEncumbrance { get; set; }
+    public required PropertyType Type { get; set; }
+    public required PropertyPurpose Purpose { get; set; }
+}
---
+++ RealEstateAgency/RealEstateAgency.Domain/Enums/PropertyType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateAgency.Domain.Enums;
+public enum PropertyType
+{
+    Apartment,
+    House,
+    Office,
+    Warehouse,
+    LandPlot
+}
