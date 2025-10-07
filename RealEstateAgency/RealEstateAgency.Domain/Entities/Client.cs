4f00fdaa2caaed991eb381af6b1065fa655410b1
instance <zhuravlev.serj2015@yandex.ru>
2025-10-07 15:46:29 +0500

    start lab. add_client.cs

---
+++ RealEstateAgency/RealEstateAgency.Domain/Entities/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateAgency.Domain.Entities;
+public class Client
+{
+    public required string FullName { get; set; }
+    public required string PassportNumber { get; set; }
+    public required string Phone { get; set; }
+}
---
+++ RealEstateAgency/RealEstateAgency.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace RealEstateAgency.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
